b254a079e3b0051c88d187e8d445509ed9b5939c
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-10-24 22:18:55 +0530

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,59 @@
 import Image from "next/image";
-
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen  flex flex-col">
+      <header className="bg-white p-4 flex-row flex  items-center justify-between ">
+        <h1 className="text-blue-500 font-bold text-xl ml-5">Horizon</h1>
+        <nav className="flex ml-auto mr-5 text-blue-500 text-sm gap-6">
+          <Link href={"#"}>Pricing</Link>
+          <Link href={"#"}>Features</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Contact</Link>
+        </nav>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <main className="flex-1">
+        <section className="w-full bg-blue-50">
+          <div className="container px-8  py-20">
+            <div className="grid grid-cols-[1fr_600px]">
+              <div className="flex flex-col justify-center space-y-4">
+                <h1 className="text-5xl font-bold tracking-tighter text-blue-800">
+                  {" "}
+                  Manage Your Finances with Ease
+                </h1>
+                <p className="text-blue-500">
+                  Take control of your financial future with our powerful and
+                  intuitive finance management application.
+                </p>
+
+                <div className="flex w-full gap-6">
+                  <Button
+                    size={"lg"}
+                    className="bg-blue-500 hover:bg-blue-700 text-white"
+                    variant={"default"}
+                  >
+                    Get Started
+                  </Button>
+
+                  <Button className="" variant={"outline"} size={"lg"}>
+                    Learn Mre
+                  </Button>
+                </div>
+              </div>
+              <Image src={"/nft.webp"} width="300" height="300" alt="" />
+            </div>
+          </div>
+        </section>
+        <section className="w-full">
+          <div className="flex justify-center items-center mt-20">
+            <h1 className="text-4xl font-bold text-blue-800 tracking-tighter">
+              Key Features
+            </h1>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
