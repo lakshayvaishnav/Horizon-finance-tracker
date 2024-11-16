a15ac695d19cd1df3e9331341f69755fd67fa128
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2024-11-16 10:28:35 +0530

    added sorting functionality

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -19,6 +19,14 @@ import {
 } from "@/components/ui/table";
 import React, { useState } from "react";
 
+interface Transaction {
+  id: number;
+  name: string;
+  description: string;
+  amount: string;
+  date: string;
+}
+
 const smampleData = [
   {
     id: 1,
@@ -36,9 +44,57 @@ const smampleData = [
     date: "2024-11-13T10:20:26.109Z",
     userId: 1,
   },
+
+  {
+    id: 3,
+    name: "yo boi",
+    description: "go",
+    amount: 23,
+    date: "2024-11-13T10:20:33.109Z",
+    userId: 1,
+  },
 ];
 
 export default function TransactionHistory(): React.ReactElement {
+  const [data, setData] = useState(smampleData);
+  const [sortConfig, setSortConfig] = useState({
+    key: " ",
+    direction: "ascending",
+  });
+
+  const handleSort = (key: keyof Transaction) => {
+    let direction = "ascending";
+
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+
+    const sortedData = [...data].sort((a, b) => {
+      if (key === "date") {
+        return direction === "ascending"
+          ? new Date(a.date).getTime() - new Date(b.date).getTime()
+          : new Date(b.date).getTime() - new Date(a.date).getTime();
+      } else if (key === "amount") {
+        return direction === "ascending"
+          ? a.amount - b.amount
+          : b.amount - a.amount;
+      } else if (key === "description") {
+        return direction === "ascending"
+          ? a.description.localeCompare(b.description) -
+              b.description.localeCompare(a.description)
+          : b.description.localeCompare(a.description) -
+              a.description.localeCompare(b.description);
+      }
+
+      return 0;
+    });
+
+    console.log("⚠️ unsorted data : ", data);
+    console.log("✅ sorted data : ", sortedData);
+    setData(sortedData);
+    setSortConfig({ key, direction });
+  };
+
   async function handleFetch() {
     const response = await fetch("http://localhost:3000/api/bank/expenses", {
       method: "GET",
@@ -61,14 +117,29 @@ export default function TransactionHistory(): React.ReactElement {
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead>Date</TableHead>
-                <TableHead>Description</TableHead>
-                <TableHead>Amount</TableHead>
+                <TableHead
+                  className="cursor-pointer"
+                  onClick={() => handleSort("date")}
+                >
+                  Date
+                </TableHead>
+                <TableHead
+                  className="cursor-pointer"
+                  onClick={() => handleSort("description")}
+                >
+                  Description
+                </TableHead>
+                <TableHead
+                  className="cursor-pointer"
+                  onClick={() => handleSort("amount")}
+                >
+                  Amount
+                </TableHead>
                 <TableHead>Name</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {smampleData.map((data) => (
+              {data.map((data) => (
                 <TableRow key={data.id}>
                   <TableCell>{data.date.split("T")[0]}</TableCell>
                   <TableCell>{data.description}</TableCell>
